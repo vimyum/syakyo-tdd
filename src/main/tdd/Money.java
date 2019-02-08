8cfc345596b686482940b928f6a99ef9f94447db
sagaratoon <sagaratoon@gmail.com>
2019-02-08 23:58:17 +0900

    chapter-8

--- src/main/tdd/Money.java
+++ src/main/tdd/Money.java
@@ -1,10 +1,21 @@
 package tdd;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
                 && getClass().equals(money.getClass());
+    }
+
+    abstract Money times(int multiplier);
+
+    static Money dollar (int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 }
