3cfa9b701ba73fabc8389d4f631f74d446448da9
sagaratoon <sagaratoon@gmail.com>
2019-02-08 23:44:02 +0900

    chapter-7

--- test/tdd/MoneyTest.java
+++ test/tdd/MoneyTest.java
@@ -12,11 +12,12 @@ class MoneyTest {
         assertEquals(new Dollar(15), five.times(3));
     }
     @Test
-    public void testEquaity() {
+    public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 
     @Test
