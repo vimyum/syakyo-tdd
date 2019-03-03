1a59ec053cd45132ad9c2e81100043272c49601f
sagaratoon <sagaratoon@gmail.com>
2019-03-04 00:53:49 +0900

    Chapter-12

---
+++ src/main/tdd/Expression.java
@@ -0,0 +1,4 @@
+package tdd;
+
+interface Expression {
+}
--- src/main/tdd/Money.java
+++ src/main/tdd/Money.java
@@ -1,6 +1,6 @@
 package tdd;
 
-public class Money {
+public class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -33,5 +33,9 @@ public class Money {
 
     public String toString() {
         return amount + " " + currency;
+    }
+
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
