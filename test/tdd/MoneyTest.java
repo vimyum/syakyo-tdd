d9da27ecce6391a03cd8d743499d393dc0952b73
sagaratoon <sagaratoon@gmail.com>
2019-03-06 23:06:16 +0900

    Chapter-14

--- test/tdd/MoneyTest.java
+++ test/tdd/MoneyTest.java
@@ -56,4 +56,18 @@ class MoneyTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testReduceMoneyDifferenceCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testIdenntifyRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
+
 }
